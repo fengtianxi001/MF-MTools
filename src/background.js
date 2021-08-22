384d54ae3c50375d56e1c618f040ae2f6d6d3845
肛肠科冯主任 <625362207@qq.com>
2021-08-22 23:54:20 +0800

    update

--- src/background.js
+++ src/background.js
@@ -1,20 +1,19 @@
 'use strict'
-
-import { app, protocol, BrowserWindow } from 'electron'
+const path = require('path')
+import { app, Tray, BrowserWindow, Menu, globalShortcut } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
-
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 // Scheme must be registered before the app is ready
-protocol.registerSchemesAsPrivileged([
-	{ scheme: 'app', privileges: { secure: true, standard: true } },
-])
+Menu.setApplicationMenu(null)
+let isShow = true
+let appTray = null
 async function createWindow() {
 	// Create the browser window.
 	const win = new BrowserWindow({
 		width: 900,
 		height: 505,
-
 		webPreferences: {
 			// Use pluginOptions.nodeIntegration, leave this alone
 			// See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
@@ -24,14 +23,28 @@ async function createWindow() {
 	})
 
 	if (process.env.WEBPACK_DEV_SERVER_URL) {
-		// Load the url of the dev server if in development mode
 		await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
 		if (!process.env.IS_TEST) win.webContents.openDevTools()
 	} else {
 		createProtocol('app')
-		// Load the index.html when not in development
 		win.loadURL('app://./index.html')
 	}
+	setTray(win)
+	win.on('minimize', function(event) {
+		event.preventDefault()
+		win.hide()
+		isShow = false
+	})
+	globalShortcut.register('space+ctrl', () => {
+		if (isShow) {
+			win.hide()
+			isShow = false
+		} else {
+			win.show()
+			isShow = true
+		}
+	})
+
 }
 
 // Quit when all windows are closed.
@@ -77,4 +90,28 @@ if (isDevelopment) {
 			app.quit()
 		})
 	}
+}
+
+function setTray(mainWindow) {
+	let trayMenuTemplate = [
+		{
+			label: '退出',
+			click: function() {
+				app.quit()
+			},
+		},
+	]
+	// 当前目录下的app.ico图标
+	let iconPath = path.resolve('./favicon.png')
+	appTray = new Tray(iconPath)
+	// 图标的上下文菜单
+	const contextMenu = Menu.buildFromTemplate(trayMenuTemplate)
+	mainWindow.hide()
+	appTray.setToolTip('mtools')
+	appTray.setContextMenu(contextMenu)
+	appTray.on('click', function() {
+		mainWindow.show()
+		// appTray.destroy()
+		isShow = true
+	})
 }
