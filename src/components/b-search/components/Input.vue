48db10751cf9b37b8196a651a3ee5e5314240720
肛肠科冯主任 <625362207@qq.com>
2022-05-13 10:52:14 +0800

    Feat: 完善search-input事件

--- src/components/b-search/components/Input.vue
+++ src/components/b-search/components/Input.vue
@@ -1,25 +1,29 @@
 <template>
   <el-form-item :label="props.config.label">
     <el-input
-      :modelValue="props?.modelValue"
+      :model-value="props?.modelValue"
       :placeholder="props.config.props?.placeholder || '请输入'"
-      @update:modelValue="onInput"
+      @keydown.enter="onresult"
+      @update:model-value="onInput"
     />
   </el-form-item>
 </template>
+
 <script setup lang="ts">
-import { InputPropsType, InputConfigType } from '../type';
+import { InputPropsType, InputConfigType } from "../type";
 interface propsType {
-  modelValue: InputPropsType['modelValue'];
+  modelValue: InputPropsType["modelValue"];
   config: InputConfigType;
+  onresult: () => void;
 }
 const props = defineProps<propsType>();
 const emit = defineEmits<{
-  (e: 'update:modelValue', value?: string): void;
+  (e: "update:modelValue", value?: string): void;
 }>();
 const onInput = (value: string) => {
-  emit('update:modelValue', value);
+  console.log(1);
+  emit("update:modelValue", value);
 };
-
+// const onChange = () => {}
 </script>
 <style scoped lang="scss"></style>
